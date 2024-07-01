1c32b2262c0eefd976384583e2c1fff566b3ff9e
Torra <noah.dpschf@gmail.com>
2024-07-01 01:58:04 -0400

    CREATE RENDERER STATIC LIB
    Create renderer (empty) static lib for in-game rendering

---
+++ ion/engine/renderer/include/Buffer.h
@@ -0,0 +1,41 @@
+
+/*
+
+ _____                               _
+|_   _|                             (_)
+  | |  ___  _ __     ___ _ __   __ _ _ _ __   ___
+  | | / _ \| '_ \   / _ \ '_ \ / _` | | '_ \ / _ \
+ _| || (_) | | | | |  __/ | | | (_| | | | | |  __/
+ \___/\___/|_| |_|  \___|_| |_|\__, |_|_| |_|\___|
+                                __/ |
+                               |___/
+
+
+NAME: Buffer.h
+
+DESCTIPTION: Buffer class for OpenGL 4.5
+
+AUTHOR: Noah de Pischof | @torrra on GitHub
+
+
+*/
+
+#pragma once
+
+#include <type_traits>
+
+#include "glad/glad.h"
+
+#define ION_VERTEX_BUFFER           GL_ARRAY_BUFFER
+#define ION_INDEX_BUFFER            GL_ELEMENT_ARRAY_BUFFER
+
+namespace ion
+{
+
+    template <GLenum TBufferEnum>
+    class Buffer
+    {
+
+    };
+
+}
---
+++ ion/engine/renderer/source/delete.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "Buffer.h"
+
+int trash(void)
+{
+	ion::Buffer<ION_VERTEX_BUFFER> a;
+	std::cout << "sup\n";
+	return 1;
+}
