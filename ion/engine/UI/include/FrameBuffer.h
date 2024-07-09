a927d68ff0c7849a3a14e32d1f8facab9da9eb8e
MLev29 <ml.290620056@gmail.com>
2024-07-09 22:53:53 +0300

    FORMAT FRAME BUFFER FILE

--- ion/engine/UI/include/FrameBuffer.h
+++ ion/engine/UI/include/FrameBuffer.h
@@ -1,3 +1,24 @@
+/*
+
+ _____                               _
+|_   _|                             (_)
+  | |  ___  _ __     ___ _ __   __ _ _ _ __   ___
+  | | / _ \| '_ \   / _ \ '_ \ / _` | | '_ \ / _ \
+ _| || (_) | | | | |  __/ | | | (_| | | | | |  __/
+ \___/\___/|_| |_|  \___|_| |_|\__, |_|_| |_|\___|
+								__/ |
+							   |___/
+
+
+NAME: FrameBuffer.h
+
+DESCTIPTION: Framebuffer header
+
+AUTHOR: @MLev29 on GitHub
+
+
+*/
+
 #pragma once
 
 namespace ion
@@ -13,8 +34,8 @@ namespace ion
 
 		unsigned int GetFrameTexture(void) const noexcept;
 		unsigned int GetFrameBuffer(void) const noexcept;
+		
 		void RescaleFrameBuffer(GLsizei width, GLsizei height);
-
 		void Bind(void) const;
 		void UnBind(void) const;
 	private:
