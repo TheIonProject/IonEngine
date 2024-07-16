c71d2a66c08d83204cc4274e368a37ab3c4e62a8
Torra <noah.dpschf@gmail.com>
2024-07-15 20:20:38 -0400

    CREATE ENTITY MANAGER
    Create entity manager class with static arrays of entities
    and components

---
+++ ion/engine/core/include/EntityManager.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+#include "Component.h"
+#include "ComponentGeneric.h"
+#include "ComponentArrayGeneric.h"
+//#include "Component.h"
+
+#include "Entity.h"
+#include "Transform.h"
+
+#include <iostream>
+
+
+namespace ion
+{
+
+	class EntityManager
+	{
+	public:
+
+		static Entity& CreateEntity(void);
+		static Entity& CreateChildEntity(const Entity& parent);
+
+		template <CComponent TComponentType> inline static
+		TComponentType& CreateComponent(const Entity& entity);
+
+		template <CComponent TComponentType> inline  static
+		TComponentType& GetComponentFromIndex(int32_t index);
+
+
+		template <CComponent TComponentType> inline static
+		TComponentType& GetEntityComponent(const Entity& entity);
+
+		static Entity& GetEntity(int32_t index);
+
+	private:
+
+		static std::vector<Entity>			m_entities;
+		static ComponentArray<Transform>	m_transformArray;
+
+
+	};
+
+
+
+
+
+	template<>
+	inline Transform& EntityManager::CreateComponent<Transform>(const Entity& entity)
+	{
+		m_transformArray.MapEntityToComponent(entity.Index(), m_transformArray.Size());
+
+		int32_t parentID = entity.Parent();
+
+		if (parentID == IComponent::EPA_INVALID_PARENT)
+			return m_transformArray.EmplaceBack(entity.Index(), parentID);
+
+		Entity& parentEntity = m_entities[parentID];
+
+		if (parentEntity.Flags() & IComponent::ECT_TRANSFORM)
+		{
+			int32_t parentComponent = m_transformArray.GetEntityComponentIndex(parentID);
+
+			return m_transformArray.EmplaceBack(entity.Index(), parentComponent);
+		}
+
+		return m_transformArray.EmplaceBack(entity.Index(), IComponent::EPA_INVALID_PARENT);
+
+	}
+
+	template<>
+	inline Transform& EntityManager::GetComponentFromIndex(int32_t index)
+	{
+		return m_transformArray.GetComponentFromIndex(index);
+	}
+
+	template<>
+	inline Transform& EntityManager::GetEntityComponent<Transform>(const Entity& entity)
+	{
+		return m_transformArray.GetEntityComponent(entity.Index());
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ ion/engine/core/source/EntityManager.cpp
@@ -0,0 +1,34 @@
+#include "EntityManager.h"
+#include "IonDebug.hpp"
+
+namespace ion
+{
+    std::vector<Entity>			EntityManager::m_entities;
+    ComponentArray<Transform>	EntityManager::m_transformArray;
+
+
+    Entity& EntityManager::CreateEntity(void)
+    {
+        int32_t     index = static_cast<int32_t>(m_entities.size());
+
+        return m_entities.emplace_back(index);
+
+    }
+
+
+    Entity& EntityManager::GetEntity(int32_t index)
+    {
+        ION_ASSERT(index != IComponent::EPA_INVALID_PARENT);
+        ION_ASSERT(index < m_entities.size());
+
+        return m_entities[index];
+    }
+
+
+    Entity& EntityManager::CreateChildEntity(const Entity& parent)
+    {
+        int32_t     index = static_cast<int32_t>(m_entities.size());
+
+        return m_entities.emplace_back(index, parent.Index());
+    }
+}
