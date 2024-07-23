8aaaf86e18d054ed5595d11a190ac98ecb3d4e7c
Torra <noah.dpschf@gmail.com>
2024-07-23 15:07:20 -0400

    CREATE SMART REF CLASS
    Create reference class that auto-refreshes when
    allocating new memory for entities and components

--- ion/engine/core/include/Entity.h
+++ ion/engine/core/include/Entity.h
@@ -1,13 +1,21 @@
 #pragma once
 
-#include <tuple>
-
-#include "Component.h"
-#include "ComponentGeneric.h"
 //#include "Component.h"
+#include "ComponentGeneric.h"
+//#include "SmartRef.h"
+//#include "Component.h"
+
+
+
 
 namespace ion
 {
+	template<typename TRefType>
+	class SmartRef;
+
+	template <CComponent TComponentType>
+	class ComponentArray;
+
 	class Entity
 	{
 	public:
@@ -30,6 +38,9 @@ namespace ion
 		template <CComponent TComponentType>
 		TComponentType&		AddComponent();
 
+		template<CComponent TComponentType> inline
+		SmartRef<TComponentType> GetSmartComponent(void);
+
 	private:
 
 		IComponent::FlagType	m_componentFlags = 0;
@@ -37,6 +48,199 @@ namespace ion
 		int32_t					m_parentIndex = IComponent::EPA_INVALID_PARENT;
 
 	};
+
+
+	template<typename TRefType>
+	class SmartRef
+	{
+	private:
+
+		inline void Refresh(void);
+		inline void SetNewIndex(int32_t index);
+
+		inline void Invalidate(void);
+
+		static inline void RefreshAll(void);
+		static inline void InvalidateAll(void);
+
+	public:
+
+		inline SmartRef(void) = delete;
+		inline SmartRef(TRefType& ref, int32_t index);
+		inline SmartRef(const SmartRef& other);
+
+		inline ~SmartRef(void);
+
+		inline TRefType* operator->(void);
+		inline TRefType& operator*(void);
+
+		inline SmartRef& operator=(const SmartRef& rhs);
+
+		inline bool operator==(const TRefType& rhs) const;
+		inline bool operator==(const SmartRef& rhs) const;
+
+	private:
+
+		TRefType*		m_ref;
+		int32_t				m_index;
+
+		static std::vector<SmartRef*>  m_allRefs;
+
+		friend class ComponentArray<TRefType>;
+
+	};
+
+
+
+	template <>
+	class SmartRef<Entity>
+	{
+	private:
+
+		SmartRef(void) = delete;
+
+
+		void Refresh(void);
+		void SetNewIndex(int32_t index);
+
+		void Invalidate(void);
+
+		static void RefreshAll(void);
+		static void InvalidateAll(void);
+
+	public:
+
+		SmartRef(Entity& ref);
+		SmartRef(const SmartRef& other);
+
+		~SmartRef(void);
+
+		Entity* operator->(void);
+		Entity& operator*(void);
+
+		SmartRef& operator=(const SmartRef& rhs);
+
+		bool operator==(const Entity& rhs) const;
+		bool operator==(const SmartRef& rhs) const;
+
+	private:
+
+		Entity* m_ref;
+		int32_t				m_index;
+
+		static std::vector<SmartRef*>  m_allRefs;
+
+		friend class EntityManager;
+	};
+
+
+
+
+
+
+	template<typename TRefType>
+	std::vector<SmartRef<TRefType>*> SmartRef<TRefType>::m_allRefs;
+
+	template<typename TRefType>
+	inline SmartRef<TRefType>::SmartRef(TRefType& ref, int32_t index)
+		: m_ref(&ref), m_index(index)
+	{
+		m_allRefs.push_back(this);
+	}
+
+	template<typename TRefType>
+	inline SmartRef<TRefType>::SmartRef(const SmartRef& other)
+		: m_ref(other.m_ref), m_index(other.m_index)
+	{
+		m_allRefs.push_back(this);
+	}
+
+	template<typename TRefType>
+	inline SmartRef<TRefType>::~SmartRef(void)
+	{
+		Invalidate();
+	}
+
+	template<typename TRefType>
+	inline void SmartRef<TRefType>::Refresh(void)
+	{
+		m_ref = /*nullptr;*/ &(EntityManager::GetComponentFromIndex<TRefType>(m_index));
+	}
+
+	template<typename TRefType>
+	inline void SmartRef<TRefType>::SetNewIndex(int32_t index)
+	{
+		m_index = index;
+		Refresh();
+	}
+
+	template<typename TRefType>
+	inline void SmartRef<TRefType>::Invalidate(void)
+	{
+		auto refIt = m_allRefs.begin();
+
+		while (refIt != m_allRefs.end())
+		{
+			if (*refIt == this)
+			{
+				m_allRefs.erase(refIt);
+				break;
+			}
+
+			++refIt;
+		}
+
+		m_index = -1;
+		m_ref = nullptr;
+	}
+
+	template<typename TRefType>
+	inline void SmartRef<TRefType>::RefreshAll(void)
+	{
+		for (SmartRef* ref : m_allRefs)
+			ref->Refresh();
+	}
+
+	template<typename TRefType>
+	inline void SmartRef<TRefType>::InvalidateAll(void)
+	{
+		for (SmartRef* ref : m_allRefs)
+			ref->Invalidate();
+	}
+
+	template<typename TRefType>
+	inline TRefType* SmartRef<TRefType>::operator->(void)
+	{
+		return m_ref;
+	}
+
+	template<typename TRefType>
+	inline TRefType& SmartRef<TRefType>::operator*(void)
+	{
+		return *m_ref;
+	}
+
+
+	template<typename TRefType>
+	inline SmartRef<TRefType>& SmartRef<TRefType>::operator=(const SmartRef& rhs)
+	{
+		m_index = rhs.m_index;
+		m_ref = rhs.m_ref;
+	}
+
+	template<typename TRefType>
+	inline bool SmartRef<TRefType>::operator==(const TRefType& rhs) const
+	{
+		return m_ref == &rhs;
+	}
+
+	template<typename TRefType>
+	inline bool SmartRef<TRefType>::operator==(const SmartRef& rhs) const
+	{
+		return m_index == rhs.m_index;
+	}
+
+
 
 
 
@@ -64,4 +268,37 @@ namespace ion
 		}
 
 	}
+
+
+	template<CComponent TComponentType>
+	inline SmartRef<TComponentType> Entity::GetSmartComponent(void)
+	{
+		int32_t			refIndex = EntityManager::GetEntityComponentIndex<TComponentType>(m_index);
+		TComponentType& component = EntityManager::GetEntityComponent<TComponentType>(*this);
+
+		return SmartRef(component, refIndex);
+
+	}
+
+
+
+
+
+
+
+
+
+
+	//std::vector<SmartRef<Entity>*> SmartRef<Entity>::m_allRefs;
+
+	//
+
+
+
+
+
+
+
+
+
 }
--- ion/engine/core/source/Entity.cpp
+++ ion/engine/core/source/Entity.cpp
@@ -36,4 +36,101 @@ namespace ion
 	{
 		return EntityManager::CreateChildEntity(*this);
 	}
+
+
+
+
+	std::vector<SmartRef<Entity>*> SmartRef<Entity>::m_allRefs;
+
+
+	SmartRef<Entity>::SmartRef(Entity& ref)
+		: m_ref(&ref), m_index(ref.Index())
+	{
+		m_allRefs.push_back(this);
+	}
+
+
+	SmartRef<Entity>::SmartRef(const SmartRef& other)
+		: m_ref(other.m_ref), m_index(other.m_index)
+	{
+		m_allRefs.push_back(this);
+	}
+
+
+	SmartRef<Entity>::~SmartRef(void)
+	{
+		Invalidate();
+	}
+
+	void SmartRef<Entity>::Refresh(void)
+	{
+		m_ref = /*nullptr;*/ &(EntityManager::GetEntity(m_index));
+	}
+
+	void SmartRef<Entity>::SetNewIndex(int32_t index)
+	{
+		m_index = index;
+		Refresh();
+	}
+
+	void SmartRef<Entity>::Invalidate(void)
+	{
+		auto refIt = m_allRefs.begin();
+
+		while (refIt != m_allRefs.end())
+		{
+			if (*refIt == this)
+			{
+				m_allRefs.erase(refIt);
+				break;
+			}
+
+			++refIt;
+		}
+
+		m_index = -1;
+		m_ref = nullptr;
+	}
+
+	void SmartRef<Entity>::RefreshAll(void)
+	{
+		for (SmartRef* ref : m_allRefs)
+			ref->Refresh();
+	}
+
+	void SmartRef<Entity>::InvalidateAll(void)
+	{
+		for (SmartRef* ref : m_allRefs)
+			ref->Invalidate();
+	}
+
+	Entity* SmartRef<Entity>::operator->(void)
+	{
+		return m_ref;
+	}
+
+	Entity& SmartRef<Entity>::operator*(void)
+	{
+		return *m_ref;
+	}
+
+
+	SmartRef<Entity>& SmartRef<Entity>::operator=(const SmartRef& rhs)
+	{
+		m_index = rhs.m_index;
+		m_ref = rhs.m_ref;
+		return *this;
+	}
+
+
+	bool SmartRef<Entity>::operator==(const Entity& rhs) const
+	{
+		return m_ref == &rhs;
+	}
+
+	bool SmartRef<Entity>::operator==(const SmartRef& rhs) const
+	{
+		return m_index == rhs.m_index;
+	}
+
 }
